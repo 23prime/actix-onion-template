8c3bc3fe51d3e2cb0340d69eead664a65853f8ac
23prime <23.prime.37@gmail.com>
2026-02-23 18:30:02 +0900

    feat(presentation): add POST /auth/login endpoint

--- app/presentation/src/lib.rs
+++ app/presentation/src/lib.rs
@@ -1,9 +1,11 @@
+pub mod auth;
 mod health;
 pub mod user;
 
 use actix_web::web;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
+    auth::configure(cfg);
     health::configure(cfg);
     user::configure(cfg);
 }
