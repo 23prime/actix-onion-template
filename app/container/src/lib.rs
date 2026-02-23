d32850ddf692f37cbd1e5be8d1bf2a59419c1a26
23prime <23.prime.37@gmail.com>
2026-02-23 18:28:04 +0900

    feat(container): add credentials_repo to Container

--- app/container/src/lib.rs
+++ app/container/src/lib.rs
@@ -1,13 +1,20 @@
 use std::sync::Arc;
 
-use domain::user::UserRepository;
+use domain::{credentials_repository::CredentialsRepository, user::UserRepository};
 
 pub struct Container {
     pub user_repo: Arc<dyn UserRepository>,
+    pub credentials_repo: Arc<dyn CredentialsRepository>,
 }
 
 impl Container {
-    pub fn new(user_repo: Arc<dyn UserRepository>) -> Self {
-        Self { user_repo }
+    pub fn new(
+        user_repo: Arc<dyn UserRepository>,
+        credentials_repo: Arc<dyn CredentialsRepository>,
+    ) -> Self {
+        Self {
+            user_repo,
+            credentials_repo,
+        }
     }
 }
